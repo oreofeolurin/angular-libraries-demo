681acb23a37430a10e94d209d2ed35c7ac04fcef
Oreofe Olurin <oolurin@diamondbank.com>
2018-12-08 01:50:55 +0100

    feat(): created library

---
+++ projects/common/src/lib/common.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cr-common',
+  template: `
+    <p>
+      common works!
+    </p>
+  `,
+  styles: []
+})
+export class CommonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
